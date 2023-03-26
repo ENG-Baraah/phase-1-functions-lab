764e3c7671e972502f49a86d82a99a47eba30b70
DESKTOP-SAWA\ASUS <eng.baraahmassri@gmail.com>
2023-03-27 00:35:29 +0300

    Function Labs Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(streetNumber) {
+  if (streetNumber < 42) return 42 - streetNumber;
+  return streetNumber - 42;
+}
+
+function distanceFromHqInFeet(streetNumber) {
+  const result = distanceFromHqInBlocks(streetNumber);
+  return result * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  if (destination > start) return (destination - start) * 264;
+  return (start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distanceInFeet = distanceTravelledInFeet(start, destination);
+  if (distanceInFeet <= 400) return 0;
+  if (distanceInFeet > 2000 && distanceInFeet < 2500) return 25;
+  if (distanceInFeet > 2500) return "cannot travel that far";
+  if (distanceInFeet > 400) return (distanceInFeet - 400) * 0.02;
+}
